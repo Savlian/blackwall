c1ddcf6301678fd4b5bd2f20ea3e2cdc7f1dee52
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2025-02-10 15:36:37 +0530

    fix all pinned message show edited

--- src/app/hooks/useRoomEvent.ts
+++ src/app/hooks/useRoomEvent.ts
@@ -12,6 +12,8 @@ const useFetchEvent = (room: Room, eventId: string) => {
     const evt = await mx.fetchRoomEvent(room.roomId, eventId);
     const mEvent = new MatrixEvent(evt);
 
+    console.log('=====> event');
+    console.log(evt.unsigned?.['m.relations'] && evt.unsigned?.['m.relations']['m.replace']);
     if (evt.unsigned?.['m.relations'] && evt.unsigned?.['m.relations']['m.replace']) {
       const replaceEvt = evt.unsigned?.['m.relations']['m.replace'] as IEvent;
       const replaceEvent = new MatrixEvent(replaceEvt);
