8f1bad2d6871e2079068653bfa625329ea90c477
Gimle Larpes <gimlelarpes@gmail.com>
2025-07-28 22:28:15 +0200

    usability improvements

--- src/app/pages/client/sidebar/SettingsTab.tsx
+++ src/app/pages/client/sidebar/SettingsTab.tsx
@@ -28,7 +28,7 @@ export function SettingsTab() {
 
   return (
     <SidebarItem active={settings}>
-      <SidebarItemTooltip tooltip={displayName}>
+      <SidebarItemTooltip tooltip="Settings">
         {(triggerRef) => (
           <SidebarAvatar as="button" ref={triggerRef} onClick={openSettings}>
             <UserAvatar
