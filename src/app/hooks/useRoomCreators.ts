88e8f73f5316ea870eb85414d1a5b140d8e3d4ad
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2025-08-11 20:38:11 +0530

    get room creators as set only if room version support them

--- src/app/hooks/useRoomCreators.ts
+++ src/app/hooks/useRoomCreators.ts
@@ -2,11 +2,14 @@ import { MatrixEvent, Room } from 'matrix-js-sdk';
 import { useMemo } from 'react';
 import { useStateEvent } from './useStateEvent';
 import { IRoomCreateContent, StateEvent } from '../../types/matrix/room';
+import { creatorsSupported } from '../utils/matrix';
 
-export const getRoomCreators = (createEvent: MatrixEvent): string[] => {
+export const getRoomCreators = (createEvent: MatrixEvent): Set<string> => {
   const createContent = createEvent.getContent<IRoomCreateContent>();
 
   const creators: Set<string> = new Set();
+
+  if (!creatorsSupported(createContent.room_version)) return creators;
 
   if (createEvent.event.sender) {
     creators.add(createEvent.event.sender);
@@ -20,13 +23,16 @@ export const getRoomCreators = (createEvent: MatrixEvent): string[] => {
     });
   }
 
-  return Array.from(creators);
+  return creators;
 };
 
-export const useRoomCreators = (room: Room): string[] => {
+export const useRoomCreators = (room: Room): Set<string> => {
   const createEvent = useStateEvent(room, StateEvent.RoomCreate);
 
-  const creators = useMemo(() => (createEvent ? getRoomCreators(createEvent) : []), [createEvent]);
+  const creators = useMemo(
+    () => (createEvent ? getRoomCreators(createEvent) : new Set<string>()),
+    [createEvent]
+  );
 
   return creators;
 };
