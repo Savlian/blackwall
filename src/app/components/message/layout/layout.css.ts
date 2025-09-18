6c869e016f87534cb4b13c8e2402f2cdb143f28d
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2025-09-18 13:20:23 +0530

    fix avatar base css

--- src/app/components/message/layout/layout.css.ts
+++ src/app/components/message/layout/layout.css.ts
@@ -120,6 +120,7 @@ export const CompactHeader = style([
 export const AvatarBase = style({
   paddingTop: toRem(4),
   transition: 'transform 200ms cubic-bezier(0, 0.8, 0.67, 0.97)',
+  display: 'flex',
   alignSelf: 'start',
 
   selectors: {
